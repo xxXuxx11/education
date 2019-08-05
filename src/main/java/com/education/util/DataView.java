5e8f1b8d96ba0bdd72eeea0800cb0a8f1c95a9c3
xxXuxx11 <52805237+xxXuxx11@users.noreply.github.com>
2019-08-05 10:54:25 +0800

    asdasd

---
+++ src/main/java/com/education/util/DataView.java
@@ -0,0 +1,33 @@
+package com.education.util;
+
+import java.util.List;
+
+public class DataView<T> {
+    private int code;
+    private String msg;
+    private List<T> data;
+
+    public int getCode() {
+        return code;
+    }
+
+    public void setCode(int code) {
+        this.code = code;
+    }
+
+    public String getMsg() {
+        return msg;
+    }
+
+    public void setMsg(String msg) {
+        this.msg = msg;
+    }
+
+    public List<T> getData() {
+        return data;
+    }
+
+    public void setData(List<T> data) {
+        this.data = data;
+    }
+}
--- src/main/java/com/education/web/LoginController.java
+++ src/main/java/com/education/web/LoginController.java
@@ -2,8 +2,10 @@ package com.education.web;
 
 import com.education.bean.Teacher;
 import com.education.service.StudentService;
+import com.education.util.DataView;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
@@ -22,8 +24,27 @@ private StudentService studentService;
         System.out.println("git");
         return teacher;
     }
+    //手机密码登录
+    @RequestMapping(method = RequestMethod.POST,value = "/user/teacher/login")
+    @ResponseBody
+    public DataView<Teacher> login(){
+        DataView<Teacher> dataView=new DataView<>();
 
 
+
+        return dataView;
+    }
+
+
+
+    //手机验证码登录
+    @RequestMapping(method = RequestMethod.POST,value = "/user/teacher/dxlogin")
+    @ResponseBody
+    public DataView<Teacher> dxlogin(){
+        DataView<Teacher> dataView=new DataView<>();
+
+        return dataView;
+    }
 
 
 }
